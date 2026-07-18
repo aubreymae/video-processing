28e27b8295910346a7d60b559b4bfdf6647c9963
Aubrey Garcia <aubreymaerg@gmail.com>
2026-07-18 11:20:06 -0400

    Added reading and saving video file locally

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+UPLOAD_FOLDER = "app/uploads"
+
 app = Flask(__name__)
 
 def create_app(config_class=Config):
@@ -7,5 +9,7 @@ def create_app(config_class=Config):
 
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix="/api")
+
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 from app import routes
--- app/api/videos.py
+++ app/api/videos.py
@@ -1,4 +1,15 @@
+# Reference: https://flask.palletsprojects.com/en/stable/patterns/fileuploads/
+
 from app.api import bp
+import os
+from flask import Flask, flash, request, redirect, url_for, current_app
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = {"mp4", "mov"}
+
+def allowed_file(filename):
+    return "." in filename and \
+    filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @bp.route("/videos/<int:id>", methods=["GET"])
 def get_video(id):
@@ -6,4 +17,16 @@ def get_video(id):
 
 @bp.route("/videos", methods=["POST"])
 def create_video():
-    pass
+    # check if post request has file part
+    if "file" not in request.files:
+        return {"error": "No file provided"}, 400
+    file = request.files["file"]
+    # if user does not select a file, browser submits an empty file
+    if file.filename == "":
+        return {"error": "No selected file provided"}, 400
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(current_app.config["UPLOAD_FOLDER"], filename))
+        return {"message": "Upload successful", "filename": filename}, 202
+    else:
+        return {"error": "File type not allowed"}, 400
