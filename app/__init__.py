4a2dcef985a48cc71013553cc0a6a275569e795e
Aubrey Garcia <aubreymaerg@gmail.com>
2026-07-19 07:32:01 -0400

    Updated app initialization by rearranging import statements and cleaning up

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,13 @@ from flask import Flask
 
 UPLOAD_FOLDER = "app/uploads"
 
-app = Flask(__name__)
-
-def create_app(config_class=Config):
-    app = Flask(__name__)
-
+def create_app():
     from app.api import bp as api_bp
-    app.register_blueprint(api_bp, url_prefix="/api")
+
+    app = Flask(__name__)
 
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-from app import routes
+    app.register_blueprint(api_bp, url_prefix="/api")
+
+    return app
