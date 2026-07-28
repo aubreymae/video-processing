cab5bd7d9ad610771be5f56236a435dff3bb0567
Aubrey Garcia <aubreymaerg@gmail.com>
2026-07-27 22:13:04 -0400

    Added error handling when processing videos with ffmpeg

--- worker.py
+++ worker.py
@@ -1,6 +1,7 @@
 # Import db model, check queue
 import sqlite3
 import time
+import subprocess, os
 
 print("3 --- WORKER STARTING; WAITING FOR JOBS")
 
@@ -31,7 +32,24 @@ while True:
         print(f"--- WORKER STARTED JOB {video_id} ---")
 
         try:
-            time.sleep(5)
+            input_file_path = f"app/uploads/"
+            output_file_path = f"app/processed/"
+
+            uploads_path = os.path.join(input_file_path, filename)
+            processed_path = os.path.join(output_file_path, f"compressed_{video_id}.mov")
+
+            if os.path.exists(uploads_path) is False:
+                raise Exception("Input file path cannot find original video.")
+
+            # CLI command
+            command = [
+                "ffmpeg",
+                "-i", uploads_path,
+                "-y",
+                processed_path
+            ]
+
+            result = subprocess.run(command, capture_output=True, text=True, check=True)
 
             # Transition to completed
             cursor.execute(update_status_query, (completed_label, video_id))
